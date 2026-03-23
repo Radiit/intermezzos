c86cc3d39bd90cd378efb0f860fa04d459bb0c6b
Ubuntu <ubuntu@localhost.localdomain>
2026-03-23 16:48:45 +0800

    Fix: Replace broken XML shortcut with clear manual steps

---
+++ IntermezzoFact.shortcut.js
@@ -0,0 +1,35 @@
+// iOS Shortcut Script - Copy this into Shortcuts app manually
+// Or use the QR code method which is easier!
+
+/*
+SIMPLE MANUAL SETUP (30 seconds):
+
+1. Open Shortcuts app on iPhone
+2. Tap + (create new)
+3. Add these 3 actions in order:
+
+ACTION 1: URL
+- Search "URL" in action search
+- Drag it in
+- Paste this URL: 
+  https://gustavoibenc.github.io/intermezzos/intermezzos.json
+
+ACTION 2: Get Dictionary Value  
+- Search "Dictionary" 
+- Drag "Get Dictionary Value" in
+- Set Key to: delivered[0].fact
+
+ACTION 3: Show Result
+- Search "Show"
+- Drag "Show Result" in
+
+4. Tap ⋯ at top
+5. Name it: "Intermezzo Fact"  
+6. Share → Add to Home Screen
+7. Done! ✨
+
+The widget will show a random verified fact each time!
+*/
+
+// That's it! No XML file needed.
+// The 3-action setup above is actually easier than importing a file.
